8182e6adf338ff8d6d4dc420328d458ad1ad127a
Cappu123 <cappyalemayehu12@gmail.com>
2026-01-25 16:14:21 +0300

    (feat): date format added

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,15 +1,25 @@
 from typing import List, Optional, Dict, Literal
 from datetime import datetime
-from pydantic import BaseModel, Field, EmailStr, Json
+from pydantic import BaseModel, Field, EmailStr, Json, field_serializer
 from uuid import UUID, uuid4
 from coreAI.AI_schemas import LLMResponse
+from utils import date_formatter
 
 #users schema
-class CreateUser(BaseModel):
+
+class ORMBase(BaseModel):
+    model_config = {"from_attributes": True}
+
+    @field_serializer("created_at", "start_date", "due_date", "updated_at", when_used="json", check_fields=False)
+    def serialize(self, value: datetime):
+        return date_formatter(value)
+    
+
+class CreateUser(ORMBase):
     email: str
     username: Optional[str]
     password: str
-class ReturnUser(BaseModel):
+class ReturnUser(ORMBase):
     id: str
     email: str
 
@@ -17,31 +27,41 @@ class ReturnUser(BaseModel):
 
 
 #Chats Schema
-class CreateChat(BaseModel):
+class CreateChat(ORMBase):
     chat_id: Optional[str] = None
     content: str
 
-class ChatResponse(BaseModel):
+class ChatResponse(ORMBase):
     chat_id: str
     reply: LLMResponse
 
-class Message(BaseModel):
+class Message(ORMBase):
     role: Literal["system", "user", "assistant"]
     content: str | dict
 
-class CreateGoal(BaseModel):
+
+class CreateGoal(ORMBase):
     goal_drafted: ChatResponse
 
-class MilestoneSteps(BaseModel):
+
+
+
+
+
+
+
+
+
+class MilestoneSteps(ORMBase):
     step: str
-class MilestoneResponse(BaseModel):
+class MilestoneResponse(ORMBase):
     id: str
     name: str
     description: str
     steps: list[MilestoneSteps]
     duration: int
     duration_unit: str
-class GoalResponse(BaseModel):
+class GoalResponse(ORMBase):
     id: str
     chat_id: str
     title: str
@@ -50,14 +70,72 @@ class GoalResponse(BaseModel):
     created_at: datetime | None
     duration: int
     duration_unit: str
-    draft_milestone: list[MilestoneResponse]
+    draft_milestone: list[MilestoneResponse]  
 
-    
-
-class GoalsResponse(BaseModel):
+class GoalsResponse(ORMBase):
     goals: list[GoalResponse]
 
 
+
+
+
+
+
+
+class StepDraft(ORMBase):
+    step_order: int
+    step_description: str
+    created_at: datetime
+    status: str
+    duration_value: int
+    duration_unit: str
+
+class StepFinal(StepDraft):
+    start_date: datetime
+    due_date: datetime
+
+class MilestoneDraft(ORMBase):
+    milestone_order: int
+    milestone_name: str
+    milestone_description: str
+    created_at: datetime 
+    status: str
+    duration_value: int
+    duration_unit: str
+    milestone_steps: list[StepDraft]
+
+class MilestoneFinal(MilestoneDraft):
+    milestone_steps: list[StepFinal]
+    start_date: datetime
+    due_date: datetime  
+
+class GoalDraft(ORMBase):
+    goal_title: str
+    goal_description: str
+    created_at: datetime
+    status: str
+    duration_value: int
+    duration_unit: str
+    milestones: list[MilestoneDraft]
+    
+class GoalFinal(GoalDraft):
+    milestones: list[MilestoneFinal]  
+    start_date: datetime
+    due_date: datetime
+
+# class GoalDraftFinal(ORMBase):
+#     goals: list[GoalDraft]
+
+# class GoalSavedFinal(ORMBase):
+#     goals: list[GoalFinal]
+
+class NotFound(ORMBase):
+    status: str
+    message: str
+    
 model_config = {
         "from_attributes": True  # Pydantic v2
     }
+
+
+
